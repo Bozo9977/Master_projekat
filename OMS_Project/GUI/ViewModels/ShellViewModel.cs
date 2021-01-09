d0d45eebe69708b35fcc228e506cdcc4be9936db
David Mrdalj <mrdalj.e516.2020@uns.ac.rs>
2021-01-09 20:41:10 +0100

    DFS drawing test

--- OMS_Project/GUI/ViewModels/ShellViewModel.cs
+++ OMS_Project/GUI/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using GUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,10 @@ namespace GUI.ViewModels
 {
     public class ShellViewModel : Screen
     {
-        private int _number1 = 0;
+        public BindableCollection<Entity> Children = new BindableCollection<Entity>();
+
+        #region Binding Test
+        /*private int _number1 = 0;
 
         public int Number1
         {
@@ -39,8 +43,48 @@ namespace GUI.ViewModels
         public string Result
         {
             get { return (Number1 + Number2).ToString(); }
+        }*/
+        #endregion
+
+        public ShellViewModel()
+        {
+            //ProcessGraph();
         }
 
+        private void ProcessGraph(Dictionary<string, Entity> D)
+        {
+            int srcColumn = 50;
+            int srcRow = 0;
 
+            DFS(D, GetFirstEntity(D), srcColumn, srcRow);
+        }
+
+        private void DFS(Dictionary<string, Entity> D, Entity u, int column, int row)
+        {
+            u.Visited = true;
+            u.Column = column;
+            u.Row = row;
+            Children.Add(u);
+
+            if (D[u.Up].Visited == false)
+                DFS(D, D[u.Up], u.Column, u.Row--);
+            else if (D[u.Left].Visited == false)
+                DFS(D, D[u.Left], u.Column--, u.Row);
+            else if (D[u.Right].Visited == false)
+                DFS(D, D[u.Right], u.Column++, u.Row);
+            else if (D[u.Down].Visited == false)
+                DFS(D, D[u.Down], u.Column, u.Row++);
+        }
+
+        private Entity GetFirstEntity(Dictionary<string, Entity> D)
+        {
+            foreach(var entity in D)
+            {
+                if (entity.Value.Up == "SRC")
+                    return entity.Value;
+            }
+
+            throw new Exception("Missing SRC connection!");
+        }
     }
 }
