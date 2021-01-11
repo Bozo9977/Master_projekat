be04b2cc4c9ca5b0552fa5ffca4f38988927d9ff
vs9216 <vs9216@protonmail.com>
2021-01-11 21:49:43 +0100

    Basic NMS host

--- OMS_Project/NMS/Program.cs
+++ OMS_Project/NMS/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,12 @@ namespace NMS
 	{
 		static void Main(string[] args)
 		{
-
+			ServiceHost host = new ServiceHost(typeof(GenericDataAccess));
+			host.Open();
+			Console.WriteLine(host.BaseAddresses[0].ToString());
+			Console.WriteLine("[Press any key to stop the service]");
+			Console.ReadKey();
+			host.Close();
 		}
 	}
 }
