eb349fb381250a84a1903f374a4bb37b186fafb9
Bozo9977 <bokimaric97@gmail.com>
2021-01-14 14:42:22 +0100

    added most basic necessities for TM

---
+++ OMS_Project/TransactionManager/ITransactionCallback.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionManager
+{
+    
+    public interface ITransactionCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void CallbackEnlist(bool prepare);
+
+        [OperationContract(IsOneWay = true)]
+        void CallbackPrepare(bool prepare);
+
+        [OperationContract(IsOneWay = true)]
+        void CallbackCommit(string commit);
+
+        [OperationContract(IsOneWay = true)]
+        void CallbackRollback(string rollback);
+    }
+}
---
+++ OMS_Project/TransactionManagerService/TransactionCallback.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionManager;
+
+namespace TransactionManagerService
+{
+    public class TransactionCallback : ITransactionCallback
+    {
+        public TransactionCallback()
+        {
+
+        }
+              
+
+        public void CallbackEnlist(bool prepare)
+        {
+            Console.WriteLine("Return for enlist.");
+        }
+
+        public void CallbackPrepare(bool prepare)
+        {
+            Console.WriteLine("Return for prepare");
+        }
+
+        public void CallbackCommit(string commit)
+        {
+            Console.WriteLine(commit);
+        }
+
+        public void CallbackRollback(string rollback)
+        {
+            Console.WriteLine(rollback);
+        }
+    }
+}
