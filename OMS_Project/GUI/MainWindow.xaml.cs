8a233a8866ca7e61cb4a06c166e724103f78b08d
David Mrdalj <mrdalj.e516.2020@uns.ac.rs>
2021-02-25 05:09:27 +0100

    Implemented Zoom/Pan functionalities + Import Schema menu option
    Logic for schema import still lacks part of implementation.

--- OMS_Project/GUI/MainWindow.xaml.cs
+++ OMS_Project/GUI/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
-﻿using NServiceBus;
+﻿using Microsoft.Win32;
+using NServiceBus;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GUI
 {
@@ -25,6 +16,10 @@ namespace GUI
         public MainWindow()
         {
             InitializeComponent();
+
+            ZoomViewbox.Width = this.Width;
+            ZoomViewbox.Height = this.Height;
+
             AsyncEndpointCreate().GetAwaiter().GetResult();
         }
 
@@ -42,5 +37,87 @@ namespace GUI
         {
             endpointInstance.Stop().ConfigureAwait(false);
         }
+
+        #region Menu
+
+        private void ImportSchema(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*txt, *.xml, *.json) | *.txt; *.xml; *.json;"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // import logic for schema
+            }
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        #region Pan
+
+        private Point _last;
+        private bool isDragged = false;
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            CaptureMouse();
+            _last = e.GetPosition(this);
+
+            isDragged = true;
+        }
+
+        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonUp(e);
+            ReleaseMouseCapture();
+            isDragged = false;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (isDragged == false)
+                return;
+
+            base.OnMouseMove(e);
+            if (e.LeftButton == MouseButtonState.Pressed && IsMouseCaptured)
+            {
+                var pos = e.GetPosition(this);
+                var matrix = mt.Matrix; // it's a struct
+                matrix.Translate((pos.X - _last.X)*5, (pos.Y - _last.Y)*5);
+                mt.Matrix = matrix;
+                _last = pos;
+            }
+        }
+
+        #endregion
+
+        #region Zoom
+
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            UpdateViewBox((e.Delta > 0) ? 50 : -50);
+        }
+
+        private void UpdateViewBox(int newValue)
+        {
+            if ((ZoomViewbox.Width > this.Width) && ZoomViewbox.Height > this.Height)
+            {
+                ZoomViewbox.Width += newValue * (this.Width / this.Height);
+                ZoomViewbox.Height += newValue;
+            }
+            else if(newValue > 0)
+            {
+                ZoomViewbox.Width += newValue * (this.Width / this.Height);
+                ZoomViewbox.Height += newValue;
+            }
+        }
+
+        #endregion
     }
 }
